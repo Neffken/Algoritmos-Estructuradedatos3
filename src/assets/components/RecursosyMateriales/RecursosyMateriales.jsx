006f6cb8df45bfdd658d11a3cc4afdb1a27d4ba2
Lautaro Barrionuevo <lauty234juan@gmail.com>
2024-11-05 20:16:24 -0300

    Correccion de titulos

--- src/assets/components/RecursosyMateriales/RecursosyMateriales.jsx
+++ src/assets/components/RecursosyMateriales/RecursosyMateriales.jsx
@@ -24,8 +24,8 @@ function RecursosyMateriales() {
 
       {/* Contenido principal */}
       <div className="content">
-         <h1 className="titulo_Recursos">Recursos y Materiales</h1>
-        <p className="recursos_Utiles">Aquí puedes encontrar recursos útiles:</p>
+         <h1 className="titulo_recursos">Recursos y Materiales</h1>
+        <p className="recursos_utiles">Aquí puedes encontrar recursos útiles:</p>
         <ul className="recursos-list">
           {recursos.map((recurso, index) => (
             <li key={index} className="recurso-item">
